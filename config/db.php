d32a3b4cb4b59ae0e6c46673914b33ba649c826a
Edward Juvenal Aguirre Garcia <eaguirre@certus.edu.pe>
2018-02-01 14:38:20 -0500

    01022018

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+define('DB_HOST', 'btssinventario.cpi1kzglqmjy.us-east-1.rds.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'bts_root');//Usuario de tu base de datos
+define('DB_PASS', 'miri0409');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'bts_inventario');//Nombre de la base de datos
 ?>
